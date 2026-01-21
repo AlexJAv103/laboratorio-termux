d225c354a8e8432c4679f8c82d6272cb51ffca4f
Alexis <ajtorres103@gmail.com>
2026-01-20 19:34:31 -0500

    Mi primer respaldo

---
+++ actualizar.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "--- Iniciando Mantenimiento del Sistema ---"
+
+# 1. Actualizar repositorios
+pkg update -y
+
+# 2. Actualizar programas instalados
+pkg upgrade -y
+
+# 3. Borrar basura (paquetes huérfanos)
+pkg autoremove -y
+
+# 4. Limpiar caché de descargas antiguas
+pkg clean
+
+echo "--- ¡Mantenimiento Completado! Tu sistema está al día. ---"
